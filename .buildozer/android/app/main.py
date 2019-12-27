0645e498159e664d1fe36188a44b274d6f00e210
Vojtech Petrasek <vojtech.petrasek@hardwario.com>
2019-12-27 19:45:23 +0100

    new rev

--- .buildozer/android/app/main.py
+++ .buildozer/android/app/main.py
@@ -4,69 +4,31 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 import json
 import requests
 
-KV = Builder.load_string ("""
-ScreenManager:
-    id: manager
-    Screen:
-        BoxLayout:
-            orientation: 'vertical'
-            GridLayout:
-                cols:1
-                Label:
-                    text: 'Write a JSON string'
-                TextInput:
-                    size_hint_y: .2
-                    id: JSON
-            GridLayout:
-                cols:1
-                Button:
-                    text: 'Patch Line'
-                    on_release: app.patch(JSON.text)
-                Button:
-                    text: 'Post Line'
-                    on_release: app.post(JSON.text)
-                Button:
-                    text: 'Put Line'
-                    on_release: app.put(JSON.text)
-                Button:
-                    text: 'Delete Line'
-                    on_release: app.delete(JSON.text)
-                Button:
-                    text: 'Get & Print Database'
-                    on_release: app.get()
-""")
-
-class MyApp(App):
+class MyGrid(Widget):
+    data = ObjectProperty(None)
 
     url = 'https://jarvis-uwuyuv.firebaseio.com/.json'
-
-    def patch(self, JSON):
-        to_database = json.loads(JSON)
-        requests.patch(url = self.url, json = to_database)
-
-    def post(self, JSON):
-        to_database = json.loads(JSON)
-        requests.post(url = self.url, json = to_database)
-
-    def put(self, JSON):
-        to_database = json.loads(JSON)
-        requests.put(url = self.url, json = to_database)
-
-    def delete(self, JSON):
-        requests.delete(url = self.url[:-5] + JSON + ".json")
-
     auth_key = 'RWnwRYHiAwVpBBkFicTdqXsUnnvnz8PyXcSJj7JP'
 
     def get(self):
         request = requests.get(self.url + '?auth=' + self.auth_key)
-        print(request.json())
+        json_data = request.json()
+        data = json_data['temp']
+        self.data.text = str(data)
+        print(data)
 
+    def clear(self):
+        self.data.text = "cleared"
+
+class MyApp(App):
     def build(self):
-        return KV
+        return MyGrid()
 
 if __name__ == '__main__':
     MyApp().run()
